96a0c5fa25751ddc8df13d0ec50b36800bf16e79
eric <whatisjava@hotmail.com>
2023-02-18 00:06:48 +0800

    update

---
+++ thread/src/org/example/thread_design_pattern/thread_specific_storage/UserInfo.java
@@ -0,0 +1,35 @@
+package org.example.thread_design_pattern.thread_specific_storage;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class UserInfo {
+    private Map<String, Object> info = new HashMap<>();
+
+    public void setId(int id) {
+        info.put("id", id);
+    }
+
+    public void setName(String name) {
+        info.put("name", name);
+    }
+
+    public void setAddress(String address) {
+        info.put("address", address);
+    }
+
+    public int getId() {
+        return (int) info.get("id");
+    }
+
+    public String getName() {
+        return (String) info.get("name");
+    }
+
+    public String getAddress() {
+        return (String) info.get("address");
+    }
+}
---
+++ thread/src/org/example/thread_design_pattern/two_phase_terminate/game/NoticeSystem.java
@@ -0,0 +1,20 @@
+package org.example.thread_design_pattern.two_phase_terminate.game;
+
+import java.util.Date;
+
+public class NoticeSystem extends GameSystem {
+
+    public NoticeSystem(String name, int refreshInterval) {
+        super(name, refreshInterval);
+    }
+
+    @Override
+    public void start() {
+        System.out.println(Thread.currentThread().getName() + "> " + new Date() + " <" + getName() + "> 正在运行中...");
+    }
+
+    @Override
+    public void finish() {
+        System.out.println("<" + getName() + "> 已关闭");
+    }
+}
